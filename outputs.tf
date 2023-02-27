0020bd56b3f1a39c82592049fed1c1800457f171
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-28 10:25:33 +1100

    Added output variables

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "samueleezy-aws-variable-set" {
+  value = tfe_variable_set.samueleezy-aws-variable-set.id
+}
+
+output "samuellee-dev-tfe-token" {
+  value = tfe_variable_set.samuellee-dev-tfe-token.id
+}
