30cee94a7e24dabe65da73f2bdea0be140bd5239
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-28 10:19:00 +1100

    Added user API token

--- tfc_variablesets.tf
+++ tfc_variablesets.tf
@@ -1,5 +1,5 @@
 resource "tfe_variable_set" "samueleezy-aws-variable-set" {
   name         = "samuelee.zy AWS variable set"
   description  = "Variable set for local aws account"
-  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.project_hcp_consul.organization
+  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.tfc_org_name
 }
