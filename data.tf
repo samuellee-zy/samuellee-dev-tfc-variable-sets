4178dff5ac296552dc835128228f99e489a07171
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-14 13:40:15 +1100

    Test

--- data.tf
+++ data.tf
@@ -8,29 +8,13 @@ data "tfe_workspace" "aws-doormat-workspace" {
   organization = var.orgName
 }
 
-data "tfe_variables" "aws-doormat-variables-access-key" {
+data "tfe_variables" "aws-doormat-variables" {
   workspace_id = var.aws-doormat-workspace-id
-  key          = "AWS_ACCESS_KEY_ID"
-}
-
-data "tfe_variables" "aws-doormat-variables-secret-key" {
-  workspace_id = var.aws-doormat-workspace-id
-  key          = "AWS_SECRET_ACCESS_KEY"
-}
-
-data "tfe_variables" "aws-doormat-variables-session-token" {
-  workspace_id = var.aws-doormat-workspace-id
-  key          = "AWS_SESSION_TOKEN"
-}
-
-data "tfe_variables" "aws-doormat-variables-session-expiration" {
-  workspace_id = var.aws-doormat-workspace-id
-  key          = "AWS_SESSION_EXPIRATION"
 }
 
 locals {
-  access_key         = data.tfe_variables.aws-doormat-variables-access-key.value
-  secret_key         = data.tfe_variables.aws-doormat-variables-secret-key.value
-  session_token      = data.tfe_variables.aws-doormat-variables-session-token.value
-  session_expiration = data.tfe_variables.aws-doormat-variables-session-expiration.value
+  access_key         = try(data.tfe_variables.workspace_vars[var.aws-doormat-workspace-id]["AWS_ACCESS_KEY_ID"].value, null)
+  secret_key         = try(data.tfe_variables.workspace_vars[var.aws-doormat-workspace-id]["AWS_SECRET_ACCESS_KEY"].value, null)
+  session_token      = try(data.tfe_variables.workspace_vars[var.aws-doormat-workspace-id]["AWS_SESSION_TOKEN"].value, null)
+  session_expiration = try(data.tfe_variables.workspace_vars[var.aws-doormat-workspace-id]["AWS_SESSION_EXPIRATION"].value, null)
 }
