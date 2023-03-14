e3a6bd6ad3b42868c422739c025f26f3cf90d2b7
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-14 12:57:58 +1100

    Updated some fix

--- tfc_variables.tf
+++ tfc_variables.tf
@@ -3,7 +3,7 @@ resource "tfe_variable" "samueleezy-aws-access-key-id" {
   value           = "INSERT-ACCESS-KEY-ID-HERE"
   category        = "env"
   description     = "Access Key ID for AWS account"
-  variable_set_id = tfe_variable_set.sa
+  variable_set_id = tfe_variable_set.samuellee-dev-aws-variable-set
 }
 
 resource "tfe_variable" "samueleezy-aws-secret-access-key" {
@@ -12,7 +12,7 @@ resource "tfe_variable" "samueleezy-aws-secret-access-key" {
   category        = "env"
   description     = "Secret Access Key for AWS account"
   sensitive       = true
-  variable_set_id = tfe_variable_set.samueleezy-aws-variable-set.id
+  variable_set_id = tfe_variable_set.samuellee-dev-aws-variable-set
 }
 
 resource "tfe_variable" "user-api-token" {
