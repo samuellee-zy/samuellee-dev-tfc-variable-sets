595ff333e2b6ce6f6824e00a11c1ced775005559
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-03-31 11:11:24 +1100

    changed organization attribute to var.OrgName

--- tfc_variablesets.tf
+++ tfc_variablesets.tf
@@ -1,25 +1,25 @@
 resource "tfe_variable_set" "samuellee-dev-aws-variable-set" {
   name         = "samuelee.zy AWS variable set"
   description  = "Variable set for local aws account"
-  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.tfc_org_name
+  organization = var.orgName
 }
 
 resource "tfe_variable_set" "samuellee-dev-tfe-token" {
   name         = "samuellee-dev tfc API token"
   description  = "Variable set containing TFE token"
-  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.tfc_org_name
+  organization = var.orgName
 }
 
 resource "tfe_variable_set" "samuellee-dev-github-token" {
   name         = "samuellee-dev github API token"
   description  = "Personal token for github"
-  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.tfc_org_name
+  organization = var.orgName
 }
 
 resource "tfe_variable_set" "samuellee-dev-hcp-iam" {
   name = "samuellee-dev HCP Client ID and Client Secret"
   description = "Demo Org Client ID and Client Secret"
-  organization = data.tfe_outputs.samuellee-dev-project-outputs.values.tfc_org_name
+  organization = var.orgName
 }
 
 
